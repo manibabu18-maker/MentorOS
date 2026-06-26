d0d70969acfe7885fb858317403a17fb1d6345be
Manibabu <manibabu.b14@gmail.com>
2026-06-26 20:57:33 +0530

    Created first MentorOS React UI

---
+++ frontend/mentoros-web/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>🚀 MentorOS</h1>
+
+      <h2>The AI Learning Operating System</h2>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
